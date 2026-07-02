c0548e006ab0c06262040b63a8ba5409f6185d9b
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-02 15:21:15 +0530

    commit

--- Factors of a number.py
+++ Factors of a number.py
@@ -1,3 +1,7 @@
 nums=int(input("The Number"))
 list=[i for i in range(1,nums+1) if nums%i==0]
 print(list)
+#Time complexity O(N)
+
+'''If we iterate upto half of Nums still it will give all factors excluding the num itself 
+and Time complexity reduces to O(N/2)'''
