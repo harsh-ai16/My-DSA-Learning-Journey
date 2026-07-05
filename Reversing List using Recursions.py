7ed46b190056e7cadd59a1cfa7a5385a709710ed
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-06 00:09:06 +0530

    Commit

--- Reversing List using Recursions.py
+++ Reversing List using Recursions.py
@@ -1,11 +1,20 @@
 nums=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def reverse(nums,left,right):
+    # if left>=right:
+    #     return nums
+    # nums[left],nums[right]=nums[right],nums[left]
+    # return reverse(nums,left+1,right-1)
+# x=(reverse(nums,0,9))
+# print(x)
     if left>=right:
-        return nums
+        return
     nums[left],nums[right]=nums[right],nums[left]
-    return reverse(nums,left+1,right-1)
+    reverse(nums,left+1,right-1)
 
-x=(reverse(nums,0,9))
-print(x)
+    
+def rev(nums,l,r):
+    reverse(nums,l,r)
+    return nums      
+print(rev(nums,0,9))
  
