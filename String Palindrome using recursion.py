a5ea5989a35829fd69dead8020c3a1de81c6a2bf
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-06 22:36:31 +0530

    commit

--- String Palindrome using recursion.py
+++ String Palindrome using recursion.py
@@ -11,3 +11,27 @@ def pali(n,l,r):
 print(pali(s,0,len(n)-1)==list(n))
 
 
+''' Using Index checking '''
+
+#using Loop
+left=0
+right=len(n)-1
+
+while left<right:
+    if n[left]!=n[right]:
+        print(False)
+        break
+    left+=1
+    right-=1
+else:
+    print(True)
+    
+#Using Recursion
+
+def func(n,left,right):
+    if left>=right:
+        return True
+    if n[left]!=n[right]:
+        return False
+    return func(n,left+1,right-1)
+print(func(n,left,right))
