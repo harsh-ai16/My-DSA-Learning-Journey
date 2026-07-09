a0e2c0dd5ae57e8e12cad65fb5027d77884dd738
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-09 21:08:01 +0530

    Commit

--- Sorting Introduction theorey.py
+++ Sorting Introduction theorey.py
@@ -1,13 +1,24 @@
 """ Selection Sort """
 n=[5,4,6,7,8,1,2,0,3,9]
 
-
 #Ascending Order 
 def sort(n):
     for i in range (0,len(n)):
         mid_index=i
         for j in range(i+1,len(n)):
             if n[j]<n[mid_index]:
+                mid_index=j
+        n[i],n[mid_index]=n[mid_index],n[i]
+sort(n)
+print(n)
+
+#Desending Sort
+
+def sort(n):
+    for i in range (0,len(n)):
+        mid_index=i
+        for j in range(i+1,len(n)):
+            if n[mid_index]<n[j]:
                 mid_index=j
         n[i],n[mid_index]=n[mid_index],n[i]
 sort(n)
