66a8979b651ead522589992b339e2e9665c49a65
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-11 21:43:50 +0530

    Commit

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,3 +1,6 @@
+""" Merge Sort """
+
+#Ascending order
 class Solution:
     def merge_arr(self,left,right):
         n,m=len(left),len(right)
@@ -32,3 +35,42 @@ class Solution:
 arr=[3,2,6,5,4,8,0,1,7]
 obj=Solution()
 print(obj.mergeSort(arr))
+
+
+#Descending Order
+class Solutions:
+    def merge_arr(self,left,right):
+        n,m=len(left),len(right)
+        i,j=0,0
+        r=[]
+        while i<n and j<m:
+            if left[i]<right[j]:
+                r.append(right[j])
+                j+=1
+            else:
+                r.append(left[i])
+                i+=1
+        while i<n:
+            r.append(left[i])
+            i+=1
+        while j<m:
+            r.append(right[j])
+            j+=1
+        return r
+    
+    def mergeSort(self,arr):
+        s=len(arr)
+        if s==1:
+            return arr
+        mid=s//2
+        left_arr=arr[:mid]
+        right_arr=arr[mid:]
+        l=self.mergeSort(left_arr)
+        r=self.mergeSort(right_arr)
+        return self.merge_arr(l,r)
+
+arr=[3,2,6,5,4,8,0,1,7]
+obj1=Solutions()
+print(obj1.mergeSort(arr))
+
+#Time Complexity is O( Nlog₂(N) ) and Space Complexity is O(N)
