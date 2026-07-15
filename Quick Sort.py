b5ce9c5d4aca1e531e1752bf39393f8d3e35a576
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-15 22:40:31 +0530

    Quick Sort

--- Quick Sort.py
+++ Quick Sort.py
@@ -1,4 +1,23 @@
 """ Quick Sort """
+class Solution:
+    def quickSort(self, arr, low, high):
+        if low < high:
+            p_ind = self.partition(arr, low, high)
+            self.quickSort(arr, low, p_ind - 1)
+            self.quickSort(arr, p_ind + 1, high)
+        return arr
 
-#Ascending Order
-def func():
+    def partition(self, arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+
+        arr[j], arr[low] = arr[low], arr[j]
+        return j
