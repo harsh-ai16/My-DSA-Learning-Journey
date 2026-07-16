fc0ea30206f31dfcc4e59bb4c53dcbdac25744e8
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-17 02:32:30 +0530

    Different methods of finding the largest

--- Finding Largest Element in an array.py
+++ Finding Largest Element in an array.py
@@ -1,10 +1,17 @@
 num=[1,3,-90,6,5,89,43,90]
 
-n=num[0]
+n=num[0]# or n = float("-inf")
 
+#Using If condition
 for i in num:
     if i>n:
         n=i
     else:
         pass
+print(n)
+
+
+#Using Max function 
+for i in num:
+    n=max(n,i)
 print(n)
