7205c105b8b08ead3ab12e457d38b1c7c7fad690
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-22 13:47:45 +0530

    Differnt approach for unsorted array

--- removing Dublicates from an array.py
+++ removing Dublicates from an array.py
@@ -32,4 +32,20 @@ print(f"No. of distinct elements {i+1} and array is {num}")
 # Time complexity is O(N) and space complexity is O(1)
 
 
+# For unsorted array
+nums1 = [3, 1, 5, 2, 3, 7, 5, 8, 1, 9]
+l=len(nums1)
+a=0
+for j in range(1,l):
+    found=False
+    for k in range(0,a+1):
+        if nums1[k]==nums1[j]:
+            found=True
+            break
+    if found==False:
+        a+=1
+        nums1[a],nums1[j]=nums1[j],nums1[a]
+
+print(f"No. of distinct elements {a+1} and array is {nums1}")
+
 
