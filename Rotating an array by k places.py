6f840a64d4234b9753b6067537490bcb98eeab7f
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-23 15:48:19 +0530

    Three reversal Approach

--- Rotating an array by k places.py
+++ Rotating an array by k places.py
@@ -6,9 +6,9 @@ k=int(input("the places to be shift"))
 n=len(nums)
 k%=n
 
-nums1=nums[n-(k):n]+nums[0:n-k]
-print(nums1)
-
+nums[:]=nums[n-(k):n]+nums[0:n-k]
+print(nums)
+#Time complexity is O(N) and space complexity id O(1)
 
 #Using Loop
 temp=nums[n-(k):n]
@@ -16,3 +16,20 @@ for i in range(n-(k+1),-1,-1):
     nums[i+k]=nums[i]
 nums[0:k]=temp
 print(nums)
+#Time complexity is O(N-k) and space complexity is O(k)
+
+#Using Three Reversal Approach
+
+def rotate(nums, k):
+    n=len(nums)
+    k%=n
+    def func(l,r):
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l+=1
+            r-=1
+
+    func(n-k,n-1)
+    func(0,n-k-1)
+    func(0,n-1)
+#Time complexity is O(N) and space complexity id O(1)
