d63805a5d2da2143c4188ac13b58e5be04c3c49e
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-25 23:02:31 +0530

    Linear Search

--- Linear Search Introduction.py
+++ Linear Search Introduction.py
@@ -1,1 +1,9 @@
 #this is the introduction to linear search approach and algorithm
+
+
+
+def search(arr, x):
+        for i in range(0,len(arr)):
+            if arr[i]==x:
+                return i
+        return -1
