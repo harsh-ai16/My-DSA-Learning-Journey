c58d423528ef21bd303956213c381a01723c92ef
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-07-27 12:21:29 +0530

    Final Made Changes

--- Merge 2 sorted arrays without duplicates.py
+++ Merge 2 sorted arrays without duplicates.py
@@ -4,33 +4,24 @@ i=0
 j=0
 newlist=[]
 while i<len(arr1) and j<len(arr2):
-    if arr1[i]==arr2[j]:
-        if len(newlist)>0 and newlist[-1]!=arr1[i]:
-            newlist.append(arr1[i])
-            
-        elif len(newlist)==0:
-            newlist.append(arr1[i])
-            
-        i+=1
-        j+=1
-    elif arr1[i]<arr2[j]:
-        if len(newlist) == 0 or newlist[-1] != arr1[i]:
-            newlist.append(arr1[i])
+    if arr1[i]<=arr2[j]:
+        if len(newlist)==0 or arr1[i]!=newlist[-1]:
+             newlist.append(arr1[i])
         i+=1
     else:
-        if len(newlist) == 0 or newlist[-1] != arr2[j]:
+        if len(newlist)==0 or arr2[j]!=newlist[-1]:
             newlist.append(arr2[j])
         j+=1
 
 while i<len(arr1):
-    if arr1[i]!=newlist[-1]:
+    if len(newlist)==0 or arr1[i]!=newlist[-1]:
         newlist.append(arr1[i])
     i+=1
         
         
 
 if j<len(arr2):
-    if arr2[j]!=newlist[-1]:
+    if len(newlist)==0 or arr2[j]!=newlist[-1]:
             newlist.append(arr2[j])
     j+=1
 
