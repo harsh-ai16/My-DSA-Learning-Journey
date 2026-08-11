63e9d50628b62dfd43a646595d51fd5d5e1e8ea3
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-08-11 23:11:07 +0530

    Better approach with two pointers

--- Three sum Problem.py
+++ Three sum Problem.py
@@ -15,3 +15,20 @@ def _3sumbrute():
     return [list(ans) for ans in result]
 print(_3sumbrute())
 # Time Complexity is O(N³) and Space xomplexity is O( Number of triplets)
+
+# Better approach (using two pointers )
+
+def _3sumbetter():
+    resultant=set()
+    for i in range(n):
+        myset=set()
+        for j in range(i+1,n):
+            k=-(nums[i]+nums[j])
+            if k in myset:
+                temp=[nums[i],nums[j],k]
+                temp.sort()
+                resultant.add(tuple(temp))
+            myset.add(nums[j])
+    return [list(ans) for ans in resultant]
+print(_3sumbetter())
+# Time complexity is O(N²) and space complexity is O(N + Number of triplets)
