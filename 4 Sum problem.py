56af0937c8a9fc4dfb18de1e2112512263f8695d
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-08-21 00:31:35 +0530

    Optimal Solution of 4sum

--- 4 Sum problem.py
+++ 4 Sum problem.py
@@ -18,6 +18,7 @@ def brute():
 print(brute())
 # Time complexity is O( N^4 ) and Space complexity is O( Number of quadruplets ) 
 
+
 # Better approach
 mysets=set()
 for i in range(n):
@@ -30,5 +31,37 @@ for i in range(n):
                 temp.sort()
                 mysets.add(tuple(temp))
             tempset.add(nums[k])
-print(mysets)
-    
+print(list(mysets))
+# Time Complexity is o(N³) and space complexity is O( N + Number of triplets )
+
+# Optimal Approach
+nums.sort()
+myset=[]
+for i in range(n):
+    if i>0 and nums[i]==nums[i-1]:
+        continue
+
+    for j in range(i+1,n):
+        if j>i+1 and nums[j]==nums[j-1]:
+            continue
+        k=j+1
+        l=n-1
+        while k<l:
+            total=nums[i]+nums[j]+nums[k]+nums[l]
+            if total < target:
+                j+=1
+            elif  total > target:
+                l-=1
+            else:
+                myset.append([nums[i],nums[j],nums[k],nums[l]])
+                j+=1
+                l=-1
+
+                while k<l and nums[k]==nums[k-1]:
+                    k+=1
+                while k<l and nums[l]==nums[l+1]:
+                    l-=1
+
+print(myset)
+                
+
