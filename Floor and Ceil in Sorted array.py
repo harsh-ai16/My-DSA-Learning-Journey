e28cde300ea7820227347c6ec658a19a70f8f000
HarshGypsy14 <harshladhiya2006@gmail.com>
2026-08-24 16:20:48 +0530

    Brute Force approach

--- Floor and Ceil in Sorted array.py
+++ Floor and Ceil in Sorted array.py
@@ -1,7 +1,7 @@
 nums = [2, 5, 8, 12, 15, 19, 23, 27, 31, 36]
 n=len(nums)
 
-target=2
+target=6
 floor=-1
 ceil=-1
 
@@ -24,3 +24,17 @@ while low<=high:
 
 print(nums[floor],nums[ceil])
 # Time complexity is  O(log₂(N)) and Space complexity is O(1)
+
+# Brute force approach
+floor1,ceil1=-1,-1
+for i in range(n):
+    if nums[i]==target:
+        floor1,ceil1=i,i
+        break
+    elif nums[i]<target:
+        floor1=i
+    else:
+        ceil1=i
+        break
+print(nums[floor1],nums[ceil1])
+# Time complexity is O(N) and space complexity is O(1)
